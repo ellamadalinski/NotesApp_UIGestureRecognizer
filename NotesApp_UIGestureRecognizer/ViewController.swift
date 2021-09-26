c4c92d5fe18fe566e3d033c402e984100b25122a
Ella Madalinski <ella.madalinski24@d155.org>
2021-09-26 12:29:03 -0500

    initial commit

--- NotesApp_UIGestureRecognizer/ViewController.swift
+++ NotesApp_UIGestureRecognizer/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var imageOutlet: UIImageView!
+    @IBOutlet weak var textFieldOutlet: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapScreenAction(_ sender: UITapGestureRecognizer) {
+        
+        view.backgroundColor = UIColor.red
+        
+        var tappedLoc = sender.location(in: view)
+        labelOutlet.center = tappedLoc
+        print("tapped")
+        textFieldOutlet.resignFirstResponder()
+        
+    }
+    
+    
+    @IBAction func panAction(_ sender: UIPanGestureRecognizer) {
+        
+        var pannedLoc = sender.location(in: view)
+        imageOutlet.center = pannedLoc //USER INTERATION ENABLED
+        print("panning")
+    }
+    
 }
 
